075de9fc4e1b372a2ba7cc49ce27cd6a9d36f948
Shine wOng <1551885@tongji.edu.cn>
2019-08-21 19:16:53 +0800

    move the definition of insert2free_list to default_pmm.c

--- lab2/kern/mm/default_pmm.h
+++ lab2/kern/mm/default_pmm.h
@@ -5,18 +5,5 @@
 
 extern const struct pmm_manager default_pmm_manager;
 
-/* search for a proper positon in free_list to place new memory block*/
-static void 
-insert2free_list(list_entry_t *elem){
-	list_entry_t *le = &free_list;
-    while((le = list_next(le)) != &free_list){
-        if(elem < le){
-            list_add_before(le, elem);
-            break;
-        }
-    }
-    if(le == &free_list) list_add_before(le, elem);
-}
-
 #endif /* ! __KERN_MM_DEFAULT_PMM_H__ */
 
