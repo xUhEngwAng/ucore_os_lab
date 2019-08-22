dabeb955f0aaa45b0a0444bf2570c7614694145e
Shine wOng <1551885@tongji.edu.cn>
2019-08-22 19:06:03 +0800

    modity Makefile command in order to enable gdb debug.

--- lab2/kern/mm/default_pmm.c
+++ lab2/kern/mm/default_pmm.c
@@ -116,7 +116,7 @@ default_alloc_pages(size_t n) {
             p->property = page->property - n;
             //list_add(&free_list, &(p->page_link));
             insert2free_list(&(p->page_link));
-    }
+        }
         nr_free -= n;
         ClearPageProperty(page);
     }
