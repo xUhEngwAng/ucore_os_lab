7201b6c70ab201c1c83d8ab7c4200586ed0735e3
Shine wOng <1551885@tongji.edu.cn>
2019-09-14 15:23:11 +0800

    modify some bugs in kern/schedule/default_sched_strde.c, most tests passed.

--- lab6/kern/schedule/default_sched_stride.c
+++ lab6/kern/schedule/default_sched_stride.c
@@ -94,7 +94,7 @@ stride_dequeue(struct run_queue *rq, struct proc_struct *proc) {
       *         skew_heap_remove: remove a entry from skew_heap
       *         list_del_init: remove a entry from the  list
       */
-      skew_heap_remove(rq->lab6_run_pool, &(proc->lab6_run_pool), proc_stride_comp_f);
+      rq->lab6_run_pool = skew_heap_remove(rq->lab6_run_pool, &(proc->lab6_run_pool), proc_stride_comp_f);
       rq->proc_num--;
 }
 /*
